b0700b697e6f7dd8f0fb63c584a845eac48aed5a
Christhian <christhianjesus@gmail.com>
2014-10-17 00:05:08 -0430

    Cambios generales

--- christhian/funciones.py
+++ christhian/funciones.py
@@ -2,8 +2,21 @@
 Created on 16/10/2014
 
 @author: Christhian
-'''
 
+Funcion: TiempoACobrar
+Entrada: 
+    estadoEstacionamiento:
+    placa: string que indica la placa del vehiculo que desea reservar.
+    tiempoSalida:
+    placaPuesto: 
+Salida:
+    unidadesReservadoNoOcupado: Tiempo reservado no ocupado
+    unidadesReservadoOcupado: Tiempo reservado Ocupado
+    unidadesOcupado: Tiempo Ocupado
+Descripcion: Al salir un cliente del estacionamiento, esta función calcula 
+el tiempo que ha permanecido estacionado independientemente de si es un cliente 
+con reserva o imprevisto.
+'''
 
 def TiempoACobrar(estadoEstacionamiento, placa, tiempoSalida, placaPuesto):
     
