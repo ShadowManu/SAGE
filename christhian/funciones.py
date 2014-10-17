65708f9ca31a16d1955b62b27cd261480a25fb6e
Christhian <christhianjesus@gmail.com>
2014-10-16 23:16:45 -0430

    hola

---
+++ christhian/funciones.py
@@ -0,0 +1,25 @@
+'''
+Created on 16/10/2014
+
+@author: Christhian
+'''
+
+
+def TiempoACobrar(estadoEstacionamiento, placa, tiempoSalida, placaPuesto):
+    
+    'puesto = placaPuesto.get(placa) = None:
+    if puesto == None:
+        retrun None
+        
+        
+    salida = {"ReservadoNoOcupado": reservadoNoOcupado,
+              "ReservadoOcupado": reservadoOcupado,
+              "Ocupado": TiempoOcupado}    #reservadoNoOcupado, reservadoOcupado y TiempoOcupado
+    return
+        
+    
+    
+    
+    
+     
+        
