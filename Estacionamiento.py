3a212bef8183e358b34d6af22508753197538166
Jesus Adolfo Parra Parra <adolfo@ldc.usb.ve>
2014-10-28 22:10:34 -0430

    Esqueleto basico de la clase Estacionamiento

---
+++ Estacionamiento.py
@@ -0,0 +1,91 @@
+'''
+Created on 28/10/2014
+
+@author: adolfo
+(Agregar a todos los demas)
+'''
+from datetime import date, datetime, time
+
+class Estacionamiento(object):
+    '''
+    Clase para los estacionamientos
+    '''
+    
+    nombreDuenio = ''
+    nombreEstacionamiento = ''
+    direccion = ''
+    telefonos = []
+    correoElectronico = []
+    rif = ''
+    capacidad = 0
+    tarifa = 0
+    horario = []
+    horarioReserva = [] 
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        pass
+        
+    def setNombreDuenio(self):
+        pass
+    
+    def getNombreDuenio(self):
+        pass
+    
+    def setNombreEstacionamiento(self):
+        pass
+    
+    def getNombreEstacionamiento(self):
+        pass
+    
+    def setDireccion(self):
+        pass
+    
+    def getDireccion(self):
+        pass
+    
+    def setTelefonos(self):
+        pass
+    
+    def getTelefonos(self):
+        pass
+    
+    def setCorreoElectronico(self):
+        pass
+    
+    def getCorreoElectronico(self):
+        pass
+    
+    def setRif(self):
+        pass
+    
+    def getRif(self):
+        pass
+    
+    def setCapacidad(self):
+        pass
+    
+    def getCapacidad(self):
+        pass
+    
+    def setTarifa(self):
+        pass
+    
+    def getTarifa(self):
+        pass
+    
+    def setHorario(self):
+        pass
+    
+    def getHorario(self):
+        pass
+    
+    def setHorarioReserva(self):
+        pass
+    
+    def getHorarioReserva(self):
+        pass
+    
+    
