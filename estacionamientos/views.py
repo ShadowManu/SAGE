b7efaa685c717863fbff95a4b7723f0d189d396f
edv862 <valderrama_862@hotmail.com>
2014-11-13 22:08:46 -0430

    antes de pull

--- estacionamientos/views.py
+++ estacionamientos/views.py
@@ -1,13 +1,11 @@
-from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.db.models import Q
 from estacionamientos.forms import EstacionamientosForm, ReservaForm
-from models import Estacionamiento, Reserva, Puesto
+from estacionamientos.models import Estacionamiento, Reserva, Puesto
 
 
 def layout(request):
-    context = RequestContext(request)
     return render_to_response('estacionamientos/layout.html')
 
 def verificarReserva(inicio, fin, cap):
