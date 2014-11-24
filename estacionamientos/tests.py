6b435741247eb54ace5d3ab6d11327a6399db2da
Jhon Villamizar <alvaro92092@gmail.com>
2014-11-24 01:27:03 -0430

    Primeras pruebas django

--- estacionamientos/tests.py
+++ estacionamientos/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+        
+    def test_basic_addition1(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+    def test_basic_addition2(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+    def test_basic_addition3(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+        
+    def test_basic_addition4(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+        
+class SimpleTest2(TestCase):
+    
+    def test_basic_addition8(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
